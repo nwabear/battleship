7e8b3091034f9dc6d79329f1ee88d4dc20a8a662
Bear Daniel <iambeardaniel@gmail.com>
2018-05-03 20:28:15 -0500

    fourth commit changes:
    --added to placePlayerShips method
    --created updateShipPlacement method
    --created isSpaceClear method
    --created clear method
    --added to main

--- src/battleship.java
+++ src/battleship.java
@@ -8,66 +8,118 @@ public class battleship {
     public static final String BLUE = "\u001B[34m";
 
     public static void main(String args[]) {
-            battleship run = new battleship();
-            String[][] playerBoard;
-            playerBoard = new String[10][10];
-            String[][] enemyBoard;
-            enemyBoard = new String[10][10];
-            String[][] attackBoard;
-            attackBoard = new String[10][10];
+        battleship run = new battleship();
+        String[][] playerBoard;
+        playerBoard = new String[10][10];
+        String[][] enemyBoard;
+        enemyBoard = new String[10][10];
+        String[][] attackBoard;
+        attackBoard = new String[10][10];
+        String[][] compareBoard = playerBoard;
 
-            run.setupBoards(playerBoard, enemyBoard, attackBoard);
-            run.displayBoards(playerBoard, attackBoard);
-        }
+        run.setupBoards(playerBoard, enemyBoard, attackBoard);
+        run.displayBoards(playerBoard, attackBoard);
+        run.placePlayerShips(playerBoard, compareBoard);
+    }
 
-        void setupBoards(String[][] playerBoard, String[][] enemyBoard, String[][] attackBoard) {
-            for(int i = 0; i < 10; i++) {
-                for(int x = 0; x < 10; x++) {
-                    playerBoard[i][x] = BLUE + "#" + RESET;
-                    enemyBoard[i][x] = BLUE + "#" + RESET;
-                    attackBoard[i][x] = BLUE + "#" + RESET;
-                }
+    void setupBoards(String[][] playerBoard, String[][] enemyBoard, String[][] attackBoard) {
+        for(int i = 0; i < 10; i++) {
+            for(int x = 0; x < 10; x++) {
+                playerBoard[i][x] = BLUE + "#" + RESET;
+                enemyBoard[i][x] = BLUE + "#" + RESET;
+                attackBoard[i][x] = BLUE + "#" + RESET;
             }
         }
+    }
 
-        void displayBoards(String[][] playerBoard, String[][] attackBoard) {
-            System.out.println("            Attack Board");
-            System.out.println("    A  B  C  D  E  F  G  H  I  J");
-            for(int y = 0; y < 10; y++) {
-                if(y != 9) {
-                    System.out.print(" " + (y + 1) + " ");
-                } else {
-                    System.out.print((y + 1) + " ");
-                }
-                for(int x = 0; x < 10; x++) {
-                    System.out.print("[" + attackBoard[x][y] + "]");
-                }
-                System.out.println("test");
+    void displayBoards(String[][] playerBoard, String[][] attackBoard) {
+        System.out.println("            Attack Board");
+        System.out.println("    A  B  C  D  E  F  G  H  I  J");
+        for(int y = 0; y < 10; y++) {
+            if(y != 9) {
+                System.out.print(" " + (y + 1) + " ");
+            } else {
+                System.out.print((y + 1) + " ");
+            }
+            for(int x = 0; x < 10; x++) {
+                System.out.print("[" + attackBoard[x][y] + "]");
             }
             System.out.println();
-            System.out.println("            Your Board");
-            System.out.println("    A  B  C  D  E  F  G  H  I  J");
-            for(int y = 0; y < 10; y++) {
-                if(y != 9) {
-                    System.out.print(" " + (y + 1) + " ");
-                } else {
-                    System.out.print((y + 1) + " ");
-                }
-                for(int x = 0; x < 10; x++) {
-                    System.out.print("[" + playerBoard[x][y] + "]");
-                }
-                System.out.println();
+        }
+        System.out.println();
+        System.out.println("            Your Board");
+        System.out.println("    A  B  C  D  E  F  G  H  I  J");
+        for(int y = 0; y < 10; y++) {
+            if(y != 9) {
+                System.out.print(" " + (y + 1) + " ");
+            } else {
+                System.out.print((y + 1) + " ");
+            }
+            for(int x = 0; x < 10; x++) {
+                System.out.print("[" + playerBoard[x][y] + "]");
+            }
+            System.out.println();
+        }
+    }
+
+    void displayBoard(String[][] board) {
+        System.out.println("    A  B  C  D  E  F  G  H  I  J");
+        for(int y = 0; y < 10; y++) {
+            if(y != 9) {
+                System.out.print(" " + (y + 1) + " ");
+            } else {
+                System.out.print((y + 1) + " ");
+            }
+            for(int x = 0; x < 10; x++) {
+                System.out.print("[" + board[x][y] + "]");
+            }
+            System.out.println();
+        }
+    }
+
+    void placePlayerShips(String[][] playerBoard, String[][] compareBoard) {
+        Scanner s = new Scanner(System.in);
+        boolean done = false;
+        int currentShip = 0;
+        int curX = 0;
+        int curY = 0;
+        int curDir = 0;
+        int[] shipLengths = {5, 4, 3, 3, 2};
+        String[] shipChars = {"A", "B", "S", "C", "D"};
+        String[] shipNames = {"Aircraft Carrier", "Battleship", "Submarine", "Cruiser", "Destroyer"};
+        while (!done) {
+            clear();
+            updateShipPlacement(compareBoard, curX, curY, curDir, shipLengths[currentShip], shipChars[currentShip]);
+            displayBoard(compareBoard);
+            System.out.println("You are placing the " + shipNames[currentShip] + ".");
+            System.out.println("Use the arrows to move the position, press D to change direction,");
+            System.out.print("and press enter to confirm");
+        }
+    }
+
+    void updateShipPlacement(String[][] board, int x, int y, int dir, int shipLength, String shipChar) {
+        for(int i = 0; i < shipLength; i++) {
+            board[x][y] = WHITE + shipChar + RESET;
+            if(dir == 0) {
+                x++;
+            } else {
+                y++;
             }
         }
+    }
 
-        void placePlayerShips() {
-            Scanner s = new Scanner(System.in);
-            boolean done = false;
-            int currentShip = 0;
-            int[] shipLengths = {5, 4, 3, 3, 2};
-            String[] shipNames = {"Aircraft Carrier","Battleship","Submarine","Cruiser","Destroyer"};
-            while(!done) {
-
-            }
+    boolean isSpaceClear(String[][] board, int x, int y, int dir, int shipLength) {
+        boolean isGood = true;
+        for(int i = 0; i < shipLength; i++) {
+            if(board[x][y] != (BLUE + "%" + RESET)) isGood = false;
+            if(x >= 10 || y >= 10) isGood = false;
+            if(dir == 0) x++;
+            else y++;
         }
+        return isGood;
+    }
+
+    void clear() {
+        for(int i = 0; i < 100; i++) System.out.println();
+    }
 }
